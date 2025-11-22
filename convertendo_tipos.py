b8a2939e78feeee05eed194d0490e7e16c7d8b2e
leoscamargo <leogne98@hotmail.com>
2025-11-22 18:50:30 -0300

    first commit

---
+++ convertendo_tipos.py
@@ -0,0 +1,22 @@
+preco = 10
+
+print(preco / 2)
+
+print (preco // 2)
+
+print(str(preco))
+
+idade = 26
+nome = 'Leonardo'
+texto = f"A idade é {idade} e o nome é {nome}"
+
+print(texto)
+
+print(int(1.98958))
+print(int("10.9"))
+print(float("10.10"))
+
+valor = 10 
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
---
+++ operadores_aritmeticos.py
@@ -0,0 +1,16 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)  # Adição
+print(produto_2 - produto_1)  # Subtração
+print(produto_1 * produto_2)  # Multiplicação
+print(produto_2 / produto_1)  # Divisão
+print(produto_1 // produto_2) # Divisão inteira
+print(produto_1 % produto_2)  # Módulo
+print(produto_1 ** produto_2) # Exponenciação
+
+x = 10 + 5 * 4
+y = 10 / 2 + 25 * 2 - 2 ** 2
+
+print(x)
+print(y)
